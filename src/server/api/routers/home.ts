e58a497b47a38400b05b12c66193447f0220ae16
Thada Wangthammang <thada.wth@gmail.com>
2025-02-20 11:46:21 +0700

    chore: remove non used code

--- src/server/api/routers/home.ts
+++ src/server/api/routers/home.ts
@@ -3,35 +3,10 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const homeRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input, ctx }) => {
-      ctx.context.log("Hello ", input.text);
-      return {
-        greeting: `Hellox ${input.text}`,
-      };
-
-    }),
-
-  create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      ctx.context.log("Create post name: ", input.name);
-      return {
-        message: `Post created: ${input.name}`,
-      }
-    }),
-
-  getLatest: publicProcedure.query(async ({ ctx }) => {
-    const post = {
-      title: "Hello XXX",
-    }
-    return post ?? null;
-  }),
 
   showGroup: publicProcedure
     .input(z.object({ userId: z.string() }))
-    .query(async ({ ctx, input }) => {
+    .query(async ({ input }) => {
       return `Group Annoucement for ${input.userId}`;
     }),
 
